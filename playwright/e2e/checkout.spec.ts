23b5f53aab1b195d0deb2626fd7c7e6172a83e1b
Tainara Reis <tainara.s.r@hotmail.com>
2026-07-10 14:26:48 -0300

    modulo 4: desafio-03 aula Fluxo E2E + Code Challenge: controle de dados no banco (#2)

--- playwright/e2e/checkout.spec.ts
+++ playwright/e2e/checkout.spec.ts
@@ -1,5 +1,7 @@
-import { test } from '../support/fixtures'
-import { CHECKOUT_ERRORS } from '../support/actions/checkoutActions'
+import { test, expect } from '../support/fixtures'
+import { CHECKOUT_ERRORS, VALID_CHECKOUT_DATA } from '../support/actions/checkoutActions'
+import { deleteOrderByNumber, deleteOrdersByEmail } from '../support/database/orderRepository'
+
 
 test.describe('Checkout - Validação de Formulário', () => {
   test.beforeEach(async ({ app }) => {
@@ -51,5 +53,43 @@ test.describe('Checkout - Validação de Formulário', () => {
 
     await app.checkout.expectError(CHECKOUT_ERRORS.termsRequired)
     await app.checkout.expectStillOnCheckout()
+  })
+})
+
+test.describe('Checkout - Fluxo Feliz', () => {
+  test('CT05 - deve realizar pedido à vista com sucesso a partir da landing page', async ({ page, app }) => {
+    const order = {
+      store: 'Velô Paulista - Av. Paulista, 1000',
+      expectedPrice: 'R$ 40.000,00',
+    } as const
+
+    //Cleanup (First option)
+    await deleteOrdersByEmail(VALID_CHECKOUT_DATA.email)
+
+    //Arrange
+    await page.goto('/')
+    await page.getByRole('link', { name: /Configure Agora/i }).click()
+    await expect(page).toHaveURL(/\/configure/)
+
+    await app.configurator.expectPrice(order.expectedPrice)
+    await app.configurator.finishConfigurator()
+
+    //Act
+    await app.checkout.fillValidCustomerData({ acceptTerms: true , includeCpf: true})
+
+    await app.checkout.selectStore(order.store)
+    await app.checkout.selectPaymentAvista()
+
+    await app.checkout.expectCheckoutPrice(order.expectedPrice)
+
+    await app.checkout.acceptTerms(true)
+    await app.checkout.submit()
+
+    //Assert
+    await app.checkout.expectOrderSuccess()
+
+    //Cleanup (Second option)
+    const orderId = await page.getByTestId('order-id').textContent() || ''
+    await deleteOrderByNumber(orderId)
   })
 })
